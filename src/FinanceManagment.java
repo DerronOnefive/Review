ff88fdb466d35d5da517f025cb92606a43adbc69
Derron Pierre <dpierre2@ggc.edu>
2024-01-30 22:20:25 -0500

    initial commit

--- src/FinanceManagment.java
+++ src/FinanceManagment.java
@@ -1,2 +1,24 @@
-public class Room {
+import java.util.Scanner;
+
+public class FinanceManagement {
+
+    final static double MONTHLY_RATE = 0.05 / 12;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the monthly saving amount:");
+        double monthlySaving = scanner.nextDouble();
+
+        computeSavingValues(monthlySaving, 6, MONTHLY_RATE);
+    }
+
+    private static void computeSavingValues(double monthlySaving, int months, double monthlyRate) {
+        double currentBalance = 0;
+
+        for (int i = 1; i <= months; i++) {
+            currentBalance = (currentBalance + monthlySaving) * (1 + monthlyRate);
+            System.out.printf("Month %d: %.2f%n", i, currentBalance);
+        }
+    }
 }
--- src/Paint.java
+++ src/Paint.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class Paint {
+import java.util.Scanner;
+
+public class Paint {
+    private double length;
+    private double width;
+    private double height;
+    private int numWindows;
+    private int numDoors;
+
+    // Constructor
+    public paint (double length, double width, double height, int numWindows, int numDoors) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+        this.numWindows = numWindows;
+        this.numDoors = numDoors;
+    }
+
+    // Calculate the total area to be painted
+    public double calculatePaintableArea() {
+        double wallArea = 2 * (length + width) * height;
+        double windowArea = numWindows * 15;
+        double doorArea = numDoors * 21;
+        return wallArea - windowArea - doorArea;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the length of the room in feet:");
+        double length = scanner.nextDouble();
+
+        System.out.println("Enter the width of the room in feet:");
+        double width = scanner.nextDouble();
+
+        System.out.println("Enter the height of the room in feet:");
+        double height = scanner.nextDouble();
+
+        System.out.println("Enter the number of windows:");
+        int numWindows = scanner.nextInt();
+
+        System.out.println("Enter the number of doors:");
+        int numDoors = scanner.nextInt();
+
+        Paint roomAndPaint = new Paint(length, width, height, numWindows, numDoors);
+
+        double paintableArea = roomAndPaint.calculatePaintableArea();
+        int gallonsNeeded = (int) Math.ceil(paintableArea / 350);
+        double quartsNeeded = (paintableArea % 350) / 350 * 4;
+
+        System.out.printf("To paint the room, you will need %d gallons and %.2f quarts%n", gallonsNeeded, quartsNeeded);
+    }
 }
